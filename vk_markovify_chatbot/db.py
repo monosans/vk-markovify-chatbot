920ff1f75f21c48bf25ab0950e0a215e19b7f9a0
monosans <hsyqixco@protonmail.com>
2023-01-01 14:53:12 +0300

    Enable SQLite autocommit

--- vk_markovify_chatbot/db.py
+++ vk_markovify_chatbot/db.py
@@ -4,23 +4,23 @@ import aiosqlite
 
 from .text import clean_text
 
-DB = "db.sqlite3"
+
+def connect() -> aiosqlite.Connection:
+    return aiosqlite.connect("db.sqlite3", isolation_level=None)
 
 
 async def init_db() -> None:
     sql = """CREATE TABLE IF NOT EXISTS
                 history (peer_id INTEGER NOT NULL, message TEXT NOT NULL)"""
-    async with aiosqlite.connect(DB) as con:
+    async with connect() as con:
         await con.execute(sql)
-        await con.commit()
 
 
 async def get_history(peer_id: int) -> str:
     sql = "SELECT message FROM history WHERE peer_id = ?"
     params = (peer_id,)
-    async with aiosqlite.connect("db.sqlite3") as con:
-        res = await con.execute(sql, params)
-        rows = await res.fetchall()
+    async with connect() as con:
+        rows = await con.execute_fetchall(sql, params)
     return "\n".join(row[0] for row in rows)
 
 
@@ -28,14 +28,12 @@ async def add_to_history(peer_id: int, *, message: str) -> None:
     message = clean_text(message)
     sql = "INSERT INTO history (peer_id, message) VALUES (?, ?)"
     params = (peer_id, message)
-    async with aiosqlite.connect(DB) as con:
+    async with connect() as con:
         await con.execute(sql, params)
-        await con.commit()
 
 
 async def clean_history(peer_id: int) -> None:
     sql = "DELETE FROM history WHERE peer_id = ?"
     params = (peer_id,)
-    async with aiosqlite.connect(DB) as con:
+    async with connect() as con:
         await con.execute(sql, params)
-        await con.commit()
