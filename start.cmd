4884a12008ca6b1d288501ed071679748b3a45d6
monosans <hsyqixco@protonmail.com>
2023-01-01 18:29:42 +0300

    Use short versions of pip flags

--- start.cmd
+++ start.cmd
@@ -1,3 +1,3 @@
-python -m pip install --upgrade --no-cache-dir --disable-pip-version-check pip setuptools wheel; python -m pip install --requirement requirements.txt --upgrade --no-cache-dir --disable-pip-version-check
+python -m pip install -U --no-cache-dir --disable-pip-version-check pip setuptools wheel; python -m pip install -r requirements.txt -U --no-cache-dir --disable-pip-version-check
 python -m vk_markovify_chatbot
 Pause
