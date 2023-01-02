07c8c2588147bc849f2ad142fa01f5062009136d
monosans <hsyqixco@protonmail.com>
2023-01-02 09:53:44 +0300

    Small improvements

--- vk_markovify_chatbot/__main__.py
+++ vk_markovify_chatbot/__main__.py
@@ -4,14 +4,6 @@ import logging
 import sys
 
 
-def setup_logging() -> None:
-    logging.basicConfig(
-        format="%(asctime)s | %(levelname)s | %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-        level=logging.INFO,
-    )
-
-
 def install_uvloop() -> None:
     if sys.implementation.name == "cpython" and sys.platform in {
         "darwin",
@@ -25,10 +17,22 @@ def install_uvloop() -> None:
             uvloop.install()
 
 
-if __name__ == "__main__":
-    setup_logging()
+def configure_logging() -> None:
+    logging.basicConfig(
+        format="%(asctime)s | %(levelname)s | %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.INFO,
+    )
+
+
+def main() -> None:
     install_uvloop()
+    configure_logging()
 
     from .bot import bot
 
     bot.run_forever()
+
+
+if __name__ == "__main__":
+    main()
