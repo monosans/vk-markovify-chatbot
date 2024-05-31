bd4954fa02c286fdef7cea4c061defafbba0a9ad
monosans <hsyqixco@protonmail.com>
2024-05-31 21:08:20 +0500

    Beautiful logging and refactoring

--- vk_markovify_chatbot/__main__.py
+++ vk_markovify_chatbot/__main__.py
@@ -3,44 +3,62 @@ from __future__ import annotations
 import asyncio
 import logging
 import sys
-from typing import Coroutine
+from typing import Callable, Coroutine
 
+import rich.traceback
+from rich.console import Console
+from rich.logging import RichHandler
 from typing_extensions import Any, TypeVar
 
 T = TypeVar("T")
 
 
-def async_run(main: Coroutine[Any, Any, T]) -> T:
+def get_async_run() -> Callable[[Coroutine[Any, Any, T]], T]:
     if sys.implementation.name == "cpython":
         try:
             import uvloop  # type: ignore[import-not-found, unused-ignore]  # noqa: PLC0415
         except ImportError:
             pass
         else:
-            if hasattr(uvloop, "run"):
-                return uvloop.run(main)  # type: ignore[no-any-return, unused-ignore]
-            uvloop.install()
-            return asyncio.run(main)
+            try:
+                return uvloop.run  # type: ignore[no-any-return, unused-ignore]
+            except AttributeError:
+                uvloop.install()
+                return asyncio.run
 
         try:
             import winloop  # type: ignore[import-not-found, unused-ignore]  # noqa: PLC0415
         except ImportError:
             pass
         else:
-            if hasattr(winloop, "run"):
-                return winloop.run(main)  # type: ignore[no-any-return, unused-ignore]
-            winloop.install()
-            return asyncio.run(main)
+            try:
+                return winloop.run  # type: ignore[no-any-return, unused-ignore]
+            except AttributeError:
+                winloop.install()
+                return asyncio.run
     if sys.platform == "win32":
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    return asyncio.run(main)
+    return asyncio.run
 
 
 def configure_logging() -> None:
+    console = Console()
+    rich.traceback.install(
+        console=console, width=None, extra_lines=0, word_wrap=True
+    )
     logging.basicConfig(
-        format="%(asctime)s | %(levelname)s | %(message)s",
+        format="%(message)s",
         datefmt=logging.Formatter.default_time_format,
         level=logging.INFO,
+        handlers=(
+            RichHandler(
+                console=console,
+                omit_repeated_times=False,
+                show_path=False,
+                rich_tracebacks=True,
+                tracebacks_extra_lines=0,
+            ),
+        ),
         force=True,
     )
 
@@ -55,4 +73,4 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    async_run(main())
+    get_async_run()(main())
